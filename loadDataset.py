2431564931a69b665d4ce0a44f126a30b37f499f
kgrabias <kgrabias@student.agh.edu.pl>
2026-03-24 22:31:42 +0100

    fix: convert data to hstack

--- loadDataset.py
+++ loadDataset.py
@@ -2,10 +2,16 @@ import numpy as np
 from sklearn.datasets import load_iris, load_diabetes
 def loadData(dataset):
 	if dataset=='iris':
-		data = load_iris(return_X_y=True)
+		X, y = load_iris(return_X_y=True)
+		data = reshapeData(X, y)
 	if dataset=='diabetes':
 		data = load_diabetes()
 	return data
+
+
+def reshapeData(X, y):
+	return np.hstack([X, y.reshape(-1, 1)])
+
 
 # For debugging
 def printIrisData():
@@ -15,5 +21,6 @@ def printIrisData():
 	target_names = ['setosa', 'versicolor', 'virginica']
 	for i in range (0,149):
 		print('data: ',data[i],'target: ',target_names[int(target[i])])
+
 
 
