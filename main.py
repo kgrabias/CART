16c4713030600cdd02d93bc81675aa0c8968d088
kgrabias <kgrabias@student.agh.edu.pl>
2026-03-24 22:44:37 +0100

    feat: overfitted tree example

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from loadDataset import loadData
 from regTrees import createTree
 
 data = loadData('iris')
-tree = createTree(data, ops=(1, 4))
+#tree = createTree(data, ops=(0, 1))    #Tree without preprunning, overfitted
+tree = createTree(data, ops=(1, 4))     #Tree with preprunning, not overfitted
 print(tree)
