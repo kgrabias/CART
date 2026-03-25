f89f1607e2d5b401c68c32a4b686b7428324f9c1
Piotr Sikora <piotrsikora@student.agh.edu.pl>
2026-03-25 17:46:53 +0100

    wizualizacja

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from loadDataset import loadData
 from regTrees import createTree, predict
+from visualize import  plot_tree
 
 dataset = loadData('iris')
 data=dataset['data']
@@ -15,3 +16,4 @@ print(f"Accuracy: {accuracy:.4f}")
 sample = np.array([[4.25, 7.9, 5.8, 0.8]], dtype=float)
 prediction = predict(tree, sample)[0]
 print('Prediction for sample ',sample,': ',dataset['target_names'][int(prediction)])
+plot_tree(tree)
